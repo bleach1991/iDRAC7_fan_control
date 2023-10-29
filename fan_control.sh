24b845d83621c28d68fe557981719c787fe89400
yxw <youxiuwen@yeah.net>
2023-10-29 22:58:59 +0800

    fix

--- fan_control.sh
+++ fan_control.sh
@@ -51,29 +51,29 @@ echo "--> Current Average CPU Temp: ${T}"
 if [ "${T}" -ge 1 ] && [ "${T}" -le 59 ]
 then
   echo "--> Setting fan speed to 10%"
-  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff $SPEED10
+  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff ${SPEED10}
 
 # If CPU temperature between 60 and 69deg C then set fans to 15%
 elif [ "${T}" -ge 60 ] && [ "${T}" -le 69 ]
 then
   echo "--> Setting fan speed to 15%"
-  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff $SPEED15
+  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff ${SPEED15}
 
 # If CPU temperature between 70 and 79deg C then set fans to 20%
 elif [ "${T}" -ge 70 ] && [ "${T}" -le 79 ]
 then
   echo "--> Setting fan speed to 20%"
-  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff $SPEED20
+  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff ${SPEED20}
 
 # If CPU temperature between 80 and 89deg C then set fans to 25%
 elif [ "${T}" -ge 80 ] && [ "${T}" -le 89 ]
 then
   echo "--> Setting fan speed to 25%"
-  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff $SPEED25
+  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff ${SPEED25}
 
 # If CPU temperature between 90 and 99deg C then set fans to 30%
 elif [ "${T}" -ge 90 ]
 then
   echo "--> Setting fan speed to 30%"
-  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff $SPEED30
+  ipmitool -I lanplus -H ${IDRAC_IP} -U ${IDRAC_USER} -P ${IDRAC_PASSWORD} raw 0x30 0x30 0x02 0xff ${SPEED30}
 fi
